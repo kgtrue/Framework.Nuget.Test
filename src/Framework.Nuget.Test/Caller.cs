22d771ddd9b1ec6fb32caa0131b5ad293b57b123
Kristian Greve True <kristian.g.true@jp.dk>
2020-11-05 21:38:47 +0100

    nuget debug test

---
+++ src/Framework.Nuget.Test/Caller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Nuget.Test
+{
+    public class Caller
+    {
+        public async Task PrintPersonsToConsole()
+        {
+            var serviceClient = new  NetCore.Nuget.Test.ApiAsyncEndpoint();
+
+            var persons = await serviceClient.GetPersonsAsObj(30);
+            foreach (var person in persons)
+            {
+                await Console.Out.WriteLineAsync(person.Name);
+            }
+        }
+    }
+}
---
+++ src/Framework.Nuget.Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Nuget.Test
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var caller = new Caller();
+            await caller.PrintPersonsToConsole();
+        }
+    }
+}
